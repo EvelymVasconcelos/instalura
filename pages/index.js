935cbe3827754907a8d4b8d58f7a66c9e90195b0
EvelymVasconcelos <evelym.vasconcelos@gmail.com>
2021-02-14 18:57:27 -0400

    final da aula 01 - criando o projeto e subindo na vercel

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+// props = {
+//   children: 'Qualquer coisa'
+// }
+
+function Title(props){
+  return(
+    <h1>{props.children}</h1>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <div><Title>My page</Title></div>
 }
